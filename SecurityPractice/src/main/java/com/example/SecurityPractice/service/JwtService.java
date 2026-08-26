df8e4af34fc0b6996a3f93644939ea26de9e6ec6
Raunak <rounand44@gmail.com>
2026-08-26 23:06:04 +0530

    started implementing jwt

--- SecurityPractice/src/main/java/com/example/SecurityPractice/service/JwtService.java
+++ SecurityPractice/src/main/java/com/example/SecurityPractice/service/JwtService.java
@@ -1,7 +1,9 @@
 package com.example.SecurityPractice.service;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +18,11 @@ public class JwtService {
     }
 
     private String createToken(Map<String, Object> claims, String username) {
-
+       return Jwts.builder()
+               .setClaims(claims)
+               .setSubject(username)
+               .setIssuedAt(new Date(System.currentTimeMillis()))
+               .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+               .signWith(getSignKey())
     }
 }
