40a94a83224ddb6ff22967c881b4bbad66f014f4
Raunak <rounand44@gmail.com>
2026-08-30 23:04:15 +0530

    added exception handling to display error message during authentication or authorization

---
+++ SecurityPractice/src/main/java/com/example/SecurityPractice/exception/GlobalExceptionHandling.java
@@ -0,0 +1,32 @@
+package com.example.SecurityPractice.exception;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleSecurityException(Exception ex)
+    {
+        ProblemDetail problemDetail=null;
+      if(ex instanceof BadCredentialsException)
+      {
+          problemDetail=ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
+          problemDetail.setProperty("access_denied_reason","Authentication Failure");
+      }
+
+      if(ex instanceof AccessDeniedException)
+      {
+          problemDetail=ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+          problemDetail.setProperty("access_denied_reason","Not Authorized");
+      }
+
+      return problemDetail;
+    }
+}
